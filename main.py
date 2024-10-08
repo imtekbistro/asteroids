80054fba67666aab1d3099377112f08125e28ba5
imtekbistro <imtekbistro@gmail.com>
2024-10-07 23:32:11 -0400

    initial code for circles

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ def main():
     pygame.init()
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
